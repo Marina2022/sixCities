1996a4701f4cdf5956453e8248083fd1b0e8673a
Marina <alexus@maranos.kirov.ru>
2022-11-02 19:52:41 +0300

    add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,26 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from "../../pages/main-page/main-page";
+import LoginPage from "../../pages/login-page/login-page";
+import Favorites from "../../pages/favorites-page/favorites";
+import RoomPage from "../../pages/room-page/room-page";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NotFound from "../../pages/not-found/not-found";
+
+type AppType = {
+  placeQuantity: number
 }
 
+function App({placeQuantity}: AppType): JSX.Element {
+  const hasAccess = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage placeQuantity={placeQuantity}/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/offer/:id" element={<RoomPage hasAccess={hasAccess}/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const settings = {
+  PLACE_QUANTITY: 315
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placeQuantity={settings.PLACE_QUANTITY} />
   </React.StrictMode>,
 );
